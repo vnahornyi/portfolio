5ba54bdc4ea4e2076c62137e71e5c3d77c1d3a6d
Vladyslav Nahornyi <vnahornyi@outlook.com>
2022-11-27 23:27:59 +0200

    fix

--- components/UI/ScrollToBottomBtn.tsx
+++ components/UI/ScrollToBottomBtn.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { IconButton, useColorModeValue } from '@chakra-ui/react';
+import { useColorModeValue } from '@chakra-ui/react';
 
 interface IScrollToBottomBtn {
     sectionNumber?: number;
@@ -8,27 +8,35 @@ interface IScrollToBottomBtn {
 }
 
 const ScrollToBottomBtn: React.FC<IScrollToBottomBtn> = ({ sectionNumber = 1, alwaysWhite }) => {
-    const handleClick = () => {
+    // const handleClick = () => {
         const _sectionNumber = sectionNumber;
         // FIXME: scroll to section
         // window.scrollTo({
         //     top: window.innerHeight * sectionNumber,
         //     behavior: 'smooth'
         // });
-    }
+    // }
+
+    // return (
+    //     <StyledScrollBottomBtn
+    //         aria-label='scroll to bottom'
+    //         zIndex={1}
+    //         onClick={handleClick}
+    //         icon={<ChevronDownIcon w='10' h='10' color={useColorModeValue(alwaysWhite ? 'black' : 'green.400', 'white')} />}
+    //     />
+    // );
 
     return (
-        <StyledScrollBottomBtn
-            aria-label='scroll to bottom'
-            zIndex={1}
-            onClick={handleClick}
-            icon={<ChevronDownIcon w='10' h='10' color={useColorModeValue(alwaysWhite ? 'black' : 'green.400', 'white')} />}
-        />
-    );
+        <StyledScrollBottomBtn>
+            <ChevronDownIcon w='10' h='10' color={useColorModeValue(alwaysWhite ? 'black' : 'green.400', 'white')} />
+        </StyledScrollBottomBtn>
+    )
 }
 
-const StyledScrollBottomBtn = styled(IconButton)`
+// const StyledScrollBottomBtn = styled(IconButton)`
+const StyledScrollBottomBtn = styled.div`
     position: absolute;
+    z-index: 1;
     bottom: 20px;
     left: 50%;
     transform: translateX(-50%);
